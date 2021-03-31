5936379750c070822b8e175f2eea5d228d1fe529
Juan Sanchez <juan.sanchez@phila.gov>
2021-03-30 22:56:05 -0400

    homepage and login page routes created

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,37 @@
 const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+//homepage route
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: { exclude: ['password'] },
+        },
+      ],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('homepage', {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//login page route
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
 
 module.exports = router;
