30334341ae67c6e0914be343d88b8946bb41d971
Juan Sanchez <juan.sanchez@phila.gov>
2021-04-01 05:46:54 -0400

    added various get routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,15 +1,29 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
-const withAuth = require('../utils/auth');
+const sequelize = require('../config/connection');
 
-//homepage route
 router.get('/', async (req, res) => {
   try {
     const postData = await Post.findAll({
+      attributes: ['id', 'title', 'content', 'date_created'],
       include: [
         {
+          model: Comment,
+          attributes: [
+            'id',
+            'comment_text',
+            'post_id',
+            'user_id',
+            'date_created',
+          ],
+          include: {
+            model: User,
+            attributes: ['username'],
+          },
+        },
+        {
           model: User,
-          attributes: { exclude: ['password'] },
+          attributes: ['username'],
         },
       ],
     });
@@ -25,13 +39,100 @@ router.get('/', async (req, res) => {
   }
 });
 
-//login page route
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
     res.redirect('/');
     return;
   }
   res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await post.findOne(req.param.id, {
+      attributes: ['id', 'content', 'title', 'date_created'],
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            'id',
+            'comment_text',
+            'post_id',
+            'user_id',
+            'date_created',
+          ],
+          include: {
+            model: User,
+            attributes: ['username'],
+          },
+        },
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+
+    const post = postData.get({ plain: true });
+
+    res.render('post', {
+      ...post,
+      logged_in: req.session.logged_in,
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get('/post_comments', async (req, res) => {
+  try {
+    const postData = await post.findOne(req.param.id, {
+      attributes: ['id', 'content', 'title', 'date_created'],
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            'id',
+            'comment_text',
+            'post_id',
+            'user_id',
+            'date_created',
+          ],
+          include: {
+            model: User,
+            attributes: ['username'],
+          },
+        },
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+
+    const post = postData.get({ plain: true });
+
+    res.render('post_comments', {
+      ...post,
+      logged_in: req.session.logged_in,
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
