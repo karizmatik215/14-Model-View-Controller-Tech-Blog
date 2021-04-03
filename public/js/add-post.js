28d09259b76f11176d4a716d87fcacaf133efeac
Juan Sanchez <juan.sanchez@phila.gov>
2021-04-02 20:29:32 -0400

    javascript for adding a post

---
+++ public/js/add-post.js
@@ -0,0 +1,27 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="post-title"]').value;
+  const content = document.querySelector('input[name="content"]').value;
+
+  const response = await fetch(`/api/posts`, {
+    method: 'POST',
+    body: JSON.stringify({
+      title,
+      content,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    alert(response.statusText);
+  } else {
+    document.location.replace('/dashboard');
+  }
+};
+
+document
+  .querySelector('#new-post-form')
+  .addEventListener('submit', newFormHandler);
