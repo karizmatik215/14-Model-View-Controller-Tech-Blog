d5caea76e18ecc703cbf5a242e9947e83d332a22
Juan Sanchez <juan.sanchez@phila.gov>
2021-04-02 20:29:48 -0400

    javascript for comments

---
+++ public/js/comment.js
@@ -0,0 +1,34 @@
+const commentFormHandler = async (event) => {
+  event.preventDefault();
+
+  const comment_text = document
+    .querySelector('input[name="comment-body"]')
+    .value.trim();
+
+  const post_id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+
+  if (comment_text) {
+    const response = await fetch('/api/comments', {
+      method: 'POST',
+      body: JSON.stringify({
+        post_id,
+        comment_text,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      alert(response.statusText);
+    } else {
+      document.location.reload();
+    }
+  }
+};
+
+document
+  .querySelector('.comment-form')
+  .addEventListener('submit', commentFormHandler);
