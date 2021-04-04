07a8ec6e7c8834aa89480db13ab04a61b4c6660a
Juan Sanchez <juan.sanchez@phila.gov>
2021-04-04 04:47:01 -0400

    modified models. removed some cascade on delete.

--- models/index.js
+++ models/index.js
@@ -13,17 +13,14 @@ Post.belongsTo(User, {
 
 Comment.belongsTo(User, {
   foreignKey: 'user_id',
-  onDelete: 'cascade',
 });
 
 Comment.belongsTo(Post, {
   foreignKey: 'post_id',
-  onDelete: 'cascade',
 });
 
 User.hasMany(Comment, {
   foreignKey: 'user_id',
-  onDelete: 'cascade',
 });
 
 Post.hasMany(Comment, {
